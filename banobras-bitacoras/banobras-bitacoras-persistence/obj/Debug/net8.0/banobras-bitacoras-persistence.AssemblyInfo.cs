5bb17e5d6288c39c3de2f83b61faabf87272c06b
antnioantnez7 <antonioantunez7@outlook.com>
2024-07-02 12:19:36 -0600

    se deja solo uno

--- banobras-bitacoras/banobras-bitacoras-persistence/obj/Debug/net8.0/banobras-bitacoras-persistence.AssemblyInfo.cs
+++ banobras-bitacoras/banobras-bitacoras-persistence/obj/Debug/net8.0/banobras-bitacoras-persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("banobras-bitacoras-persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afba64a9ddd550ae3cb19bf6c1fadda445ae1587")]
 [assembly: System.Reflection.AssemblyProductAttribute("banobras-bitacoras-persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("banobras-bitacoras-persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
